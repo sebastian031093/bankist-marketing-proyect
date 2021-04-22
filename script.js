dcfa5c066ae0c1cf09afeffbf373592f79f804fe
Sebasitan Manco Gomez <sebasmg1393@gmail.com>
2021-04-22 17:21:30 -0500

    hidden nav bar ok

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const nav = document.querySelector('.nav');
 
 const openModal = function (event) {
   event.preventDefault();
@@ -139,8 +140,32 @@ tabsContainer.addEventListener('click', function (event) {
   document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
 })
 
+//Menu fade animation
 
+//Refactoring coding: mira lo que se esta repitiendo y crea una funcion para ello ------DRY
 
+const handleHover = function (event) {
+
+  console.log(this, event.currentTarget);
+
+  if (event.target.classList.contains('nav__link')) {
+    const link = event.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(element => {
+      if (element !== link) element.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+//Call back in actions.
+//Passing "argument" into handler
+//the handler function only pass one parameter.
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 
 
